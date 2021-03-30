4d6f968b58a254aea559cc9ba9c19fb961fd2dc9
Connor Mendenhall <cmendenhall@8thlight.com>
2021-03-30 10:01:28 -0400

    Update Travis config/PG install script for bionic

--- scripts/install-postgres-11.sh
+++ scripts/install-postgres-11.sh
@@ -8,8 +8,5 @@ sudo apt-get remove -q 'postgresql-*'
 sudo apt-get update -q
 sudo apt-get install -q postgresql-11 postgresql-client-11
 sudo cp /etc/postgresql/{9.6,11}/main/pg_hba.conf
-
-echo "Restarting Postgres 11"
-sudo service postgresql restart
-
-sudo psql -c 'CREATE ROLE travis SUPERUSER LOGIN CREATEDB;' -U postgres
+sudo service postgresql status
+sudo pg_lsclusters
